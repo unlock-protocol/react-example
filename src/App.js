b13183dc3faf747f365fb95270d820c96f21346b
Julien Genestoux <julien.genestoux@gmail.com>
2019-07-10 23:20:30 +0200

    using Unlock inside of React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.unlockHandler = this.unlockHandler.bind(this)
+    this.checkout = this.checkout.bind(this)
+    this.state = {
+      locked: "pending" // there are 3 state: pending, locked and unlocked
+    }
+  }
+
+  /**
+   * When the component mounts, listen to events from unlockProtocol
+   */
+  componentDidMount() {
+    window.addEventListener("unlockProtocol", this.unlockHandler)
+  }
+
+  /**
+   * Make sure we clean things up before unmounting
+   */
+  componentWillUnmount() {
+    window.removeEventListener("unlockProtocol", this.unlockHandler)
+  }
+
+  /**
+   * Invoked to show the checkout modal provided by Unlock (optional... but convenient!)
+   */
+  checkout() {
+    window.unlockProtocol && window.unlockProtocol.loadCheckoutModal()
+  }
+
+  /**
+   * event handler
+   * @param {*} e
+   */
+  unlockHandler(e) {
+    this.setState(state => {
+      return {
+        ...state,
+        locked: e.detail
+      }
+    })
+  }
+
+  render() {
+    const { locked } = this.state
+    return (
+      <div className="App">
+        <header className="App-header">
+          {locked === "locked" && (
+            <div onClick={this.checkout} style={{ cursor: "pointer" }}>
+              Unlock me!{" "}
+              <span aria-label="locked" role="img">
+                🔒
+              </span>
+            </div>
+          )}
+          {locked === "unlocked" && (
+            <div>
+              Unlocked!{" "}
+              <span aria-label="unlocked" role="img">
+                🗝
+              </span>
+            </div>
+          )}
+        </header>
+      </div>
+    )
+  }
 }
 
 export default App;
